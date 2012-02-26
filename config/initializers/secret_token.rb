04a53dd8c1b4437a3e92ee35046bc8ee8d5e3e95
Tim Harvey <tim@literacy5.com>
2012-02-26 10:57:47 -0500

    Initial commit of empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'eb41773fc5f18c3ca7befe8a33b0e44836d592b33681ec9d2beb37f1cc1b530a09173c8a61cbd93329406480e94eca7775e6153decba9adb4150b5d7eb669d2d'
