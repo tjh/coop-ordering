c46bb1b3b798d4870978ce1d5afc67244145d15b
Tim Harvey <tim@theharveys.org>
2014-03-03 08:49:56 -0800

    Move mailer host config into ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,6 @@ FryFarmsCoop::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'www.fryfarmscoop.com' }
-
   # Enable threaded mode
   # config.threadsafe!
 
