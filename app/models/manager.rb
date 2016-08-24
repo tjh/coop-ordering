3e34f242ea43f8e5262b8f0dbb1fe4c51319cdd8
Tim Harvey <tim@theharveys.org>
2016-08-23 21:48:58 -0700

    Switch to Angi and Colleen insteaf of Lauren

---
+++ app/models/manager.rb
@@ -0,0 +1,20 @@
+class Manager
+  class << self
+
+    def full_name
+      'Angela Adams or Colleen Obergfell'
+    end
+
+    def email
+      'fryfarmscoop@gmail.com'
+    end
+
+    def address_line_one
+      '404 West Williamst Street'
+    end
+
+    def address_line_two
+      'Fort Wayne, IN 46802'
+    end
+  end
+end
